98860110c904a76110e729d1656e9af08fc09781
[awarbler] <[awoodfor@byui.edu]>
2022-11-15 14:45:23 -0600

    added some notes regarding oauth in user

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -2,17 +2,25 @@ const appConfig = require('../config/app');
 const User = require('../models/user');
 
 const loadUser = async (req, res, next) => {
+  // do we want to put this in a try ?
+  // try{if (!req.headers.authorization)next();
+  // const token = parseToken(req);
+  // const authZeroUser = await fetchAuthZeroUser(token);
+  // Lookup the user in _our_ database based on the
+  // user info we got back from Auth0.
+  //
+  // If no User exists in our database yet, create
+  // one and return it!
+  // const user = await findOrCreateUser(authZeroUser);
+  // Now we have a user. Set it on the request so we
+  // can access it in controllers \o/
+  // req.user = user ;
+  // next(); } catch(_error){next();}};
   console.log(req.headers.authorization);
   const authZeroUser = await fetchAuthZeroUser(req.headers.authorization);
   const user = await findOrCreateUser(authZeroUser);
   req.user = user;
   next();
-};
-
-const fetchAuthZeroUser = async (authorizationValue) => {
-  const response = await fetch(`${appConfig.authorizationHost}/userinfo`, {
-    headers: { Authorization: authorizationValue }
-  });
 };
 
 const findOrCreateUser = async (authZeroUserJson) => {
@@ -33,5 +41,17 @@ const findOrCreateUser = async (authZeroUserJson) => {
   });
   return newUser;
 };
+
+const fetchAuthZeroUser = async (authorizationValue) => {
+  const response = await fetch(`${appConfig.authorizationHost}/userinfo`, {
+    headers: { Authorization: authorizationValue }
+  });
+};
+
+// const parseToken = (req) => {
+//parse out the token. the token is in the authorization header like this:
+// authorization: Bearer<token>
+//return req.headers.authorization.split('')[1];
+//};
 
 module.exports = loadUser;
