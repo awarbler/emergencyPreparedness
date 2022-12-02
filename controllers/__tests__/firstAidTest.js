02136cd4085f13cda2c0814f29da8978be24e462
[awarbler] <[awoodfor@byui.edu]>
2022-12-02 07:46:11 -0600

    adding mockingoose to each test header

--- controllers/__tests__/firstAidTest.js
+++ controllers/__tests__/firstAidTest.js
@@ -1,206 +1,208 @@
-const firstAidController = require("../firstAid");
+const firstAidController = require('../firstAid');
+const mockingoose = require('mockingoose');
 const mockingoose = require('mockingoose');
 const FirstAid = require('../../models/firstAid');
-
 
 let req, res, send;
 
 beforeEach(() => {
-    send = jest.fn();
+  send = jest.fn();
 
-    req = {};
-    res = {
-        status: jest.fn(() => ({ send })),
-        json: jest.fn(),
-    };
+  req = {};
+  res = {
+    status: jest.fn(() => ({ send })),
+    json: jest.fn()
+  };
 });
 
 ///// getAllFirstAidItems() /////
-describe("getAllFirstAidItems()", () => {
-    describe("when there is no user present", () => {
-        beforeEach(() => (req.user = undefined));
+describe('getAllFirstAidItems()', () => {
+  describe('when there is no user present', () => {
+    beforeEach(() => (req.user = undefined));
 
-        it("responds with 401",  () => {
-            firstAidController.getAllFirstAidItems(req, res);
-    
-            expect(res.status).toHaveBeenCalledWith(401);
-        });
-    
-        it("responds with 'Not Authenticated'", () => {
-            firstAidController.getAllFirstAidItems(req, res);
-    
-            expect(send).toHaveBeenCalledWith("Not Authenticated");
-        });
+    it('responds with 401', () => {
+      firstAidController.getAllFirstAidItems(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
     });
-    describe("when there is a user present", () => {
+
+    it("responds with 'Not Authenticated'", () => {
+      firstAidController.getAllFirstAidItems(req, res);
+
+      expect(send).toHaveBeenCalledWith('Not Authenticated');
+    });
+  });
+  describe('when there is a user present', () => {
+    beforeEach(() => {
+      req.user = {
+        identifier: 'testUser'
+      };
+    });
+
+    describe('when firstAid array is empty', () => {
+      beforeEach(() => {
+        // data = [];
+        mockingoose(FirstAid).toReturn([], 'find');
+      });
+
+      it('responds with 200', async () => {
+        await firstAidController.getAllFirstAidItems(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+      });
+
+      it('responds with an empty array', async () => {
+        await firstAidController.getAllFirstAidItems(req, res);
+
+        expect(send).toHaveBeenCalledWith([]);
+      });
+
+      describe('when firstAid items exist', () => {
         beforeEach(() => {
-            req.user = {
-                identifier: "testUser",
-            };
-        });
-    
-        describe("when firstAid array is empty", () => {
-            beforeEach(() => {
-                // data = [];
-                mockingoose(FirstAid).toReturn([], 'find');
-            });
-    
-            it("responds with 200", async () => {
-                await firstAidController.getAllFirstAidItems(req, res);
-    
-                expect(res.status).toHaveBeenCalledWith(200);
-            });
-    
-            it("responds with an empty array", async () => {
-                await firstAidController.getAllFirstAidItems(req, res);
-    
-                expect(send).toHaveBeenCalledWith([]);
-            });
-    
-            describe("when firstAid items exist", () => {
-                beforeEach(() => {
-                    // data = [
-                    //     {
-                    //         name: "BandAid",
-                    //         quantity: "18",
-                    //         purchaseDate: "11/30/2022",
-                    //     }
-                    // ];
+          // data = [
+          //     {
+          //         name: "BandAid",
+          //         quantity: "18",
+          //         purchaseDate: "11/30/2022",
+          //     }
+          // ];
 
-                    mockingoose(FirstAid).toReturn([
-                        {
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        },
-                    ], 'find');
-                });
-    
-                it("responds with 200", async () => {
-                    await firstAidController.getAllFirstAidItems(req, res);
-    
-                    expect(res.status).toHaveBeenCalledWith(200);
-                });
-    
-                it("responds with firstAid items", async () => {
-                    await firstAidController.getAllFirstAidItems(req, res);
-    
-                    expect(send).toHaveBeenCalledWith([
-                        expect.objectContaining({
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }),
-                    ]);
-                });
-            });
+          mockingoose(FirstAid).toReturn(
+            [
+              {
+                name: 'BandAid',
+                quantity: '18',
+                purchaseDate: '11/30/2022'
+              }
+            ],
+            'find'
+          );
         });
+
+        it('responds with 200', async () => {
+          await firstAidController.getAllFirstAidItems(req, res);
+
+          expect(res.status).toHaveBeenCalledWith(200);
+        });
+
+        it('responds with firstAid items', async () => {
+          await firstAidController.getAllFirstAidItems(req, res);
+
+          expect(send).toHaveBeenCalledWith([
+            expect.objectContaining({
+              name: 'BandAid',
+              quantity: '18',
+              purchaseDate: '11/30/2022'
+            })
+          ]);
+        });
+      });
     });
+  });
 });
 
-
 ///// getFirstAidItemByName /////
-describe("getFirstAidItemByName()", () => {
-    describe("when there is no user present", () => {
-        beforeEach(() => (req.user = undefined, req.name = undefined));
+describe('getFirstAidItemByName()', () => {
+  describe('when there is no user present', () => {
+    beforeEach(() => ((req.user = undefined), (req.name = undefined)));
 
-        it("responds with 401",  () => {
-            firstAidController.getFirstAidItemByName(req, res);
-    
-            expect(res.status).toHaveBeenCalledWith(401);
-        });
-    
-        it("responds with 'Not Authenticated'", () => {
-            firstAidController.getFirstAidItemByName(req, res);
-    
-            expect(send).toHaveBeenCalledWith("Not Authenticated");
-        });
+    it('responds with 401', () => {
+      firstAidController.getFirstAidItemByName(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
     });
-    describe("when there is a user present", () => {
-        beforeEach(() => {
-            req.user = {
-                identifier: "testUser",
-            };
-        });
-    
-        // describe("when firstAid item by name is empty", () => {
-        //     beforeEach(() => {
-        //         data = [];
-        //         req.name = {
-        //             name: "BandAid",
-        //         };
-        //         mockingoose(FirstAid).toReturn([], 'find');
 
-        //         return FirstAid.find({ name: 'BandAid' }).then(doc => {
-        //             expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
-        //         });
-        //     });
-    
-        //     it("responds with 200", async () => {
-        //         await firstAidController.getFirstAidItemByName(req, res);
-    
-        //         expect(res.status).toHaveBeenCalledWith(200);
-        //     });
-    
-        //     it("responds with an empty firstAid item", async () => {
-        //         await firstAidController.getFirstAidItemByName(req, res);
-    
-        //         expect(send).toHaveBeenCalledWith([]);
-        //     });
-    
-            describe("when firstAid item exists", () => {
-                beforeEach(() => {
-                    data = [
-                        {
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }
-                    ];
-                    const testing = FirstAid.find({ name: "BandAid" }).then(doc => {
-                    expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
-                    console.log("====>", testing)
-                });
-                    // mockingoose(FirstAid).toReturn([
-                    //     {
-                    //         name: "BandAid",
-                    //         quantity: "18",
-                    //         purchaseDate: "11/30/2022",
-                    //     },
-                    // ], 'find');
-                });
+    it("responds with 'Not Authenticated'", () => {
+      firstAidController.getFirstAidItemByName(req, res);
 
-                it("responds with firstAid item", () => {
-                    data = [
-                        {
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }
-                    ];
-                    return FirstAid.find({ name: "BandAid" }).then(doc => {
-                    expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
-                    });
-                })
-    
-                it("responds with 200", async () => {
-                    await firstAidController.getFirstAidItemByName(req, res);
-    
-                    expect(res.status).toHaveBeenCalledWith(200);
-                });
-    
-                it("responds with firstAid item", async () => {
-                    await firstAidController.getFirstAidItemByName(req, res);
-    
-                    expect(send).toHaveBeenCalledWith([
-                        expect.objectContaining({
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }),
-                    ]);
-                });
-            });
-        });
+      expect(send).toHaveBeenCalledWith('Not Authenticated');
     });
+  });
+  describe('when there is a user present', () => {
+    beforeEach(() => {
+      req.user = {
+        identifier: 'testUser'
+      };
+    });
+
+    // describe("when firstAid item by name is empty", () => {
+    //     beforeEach(() => {
+    //         data = [];
+    //         req.name = {
+    //             name: "BandAid",
+    //         };
+    //         mockingoose(FirstAid).toReturn([], 'find');
+
+    //         return FirstAid.find({ name: 'BandAid' }).then(doc => {
+    //             expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
+    //         });
+    //     });
+
+    //     it("responds with 200", async () => {
+    //         await firstAidController.getFirstAidItemByName(req, res);
+
+    //         expect(res.status).toHaveBeenCalledWith(200);
+    //     });
+
+    //     it("responds with an empty firstAid item", async () => {
+    //         await firstAidController.getFirstAidItemByName(req, res);
+
+    //         expect(send).toHaveBeenCalledWith([]);
+    //     });
+
+    describe('when firstAid item exists', () => {
+      beforeEach(() => {
+        data = [
+          {
+            name: 'BandAid',
+            quantity: '18',
+            purchaseDate: '11/30/2022'
+          }
+        ];
+        const testing = FirstAid.find({ name: 'BandAid' }).then((doc) => {
+          expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
+          console.log('====>', testing);
+        });
+        // mockingoose(FirstAid).toReturn([
+        //     {
+        //         name: "BandAid",
+        //         quantity: "18",
+        //         purchaseDate: "11/30/2022",
+        //     },
+        // ], 'find');
+      });
+
+      it('responds with firstAid item', () => {
+        data = [
+          {
+            name: 'BandAid',
+            quantity: '18',
+            purchaseDate: '11/30/2022'
+          }
+        ];
+        return FirstAid.find({ name: 'BandAid' }).then((doc) => {
+          expect(JSON.parse(JSON.stringify(doc))).toMatchObject(data);
+        });
+      });
+
+      it('responds with 200', async () => {
+        await firstAidController.getFirstAidItemByName(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+      });
+
+      it('responds with firstAid item', async () => {
+        await firstAidController.getFirstAidItemByName(req, res);
+
+        expect(send).toHaveBeenCalledWith([
+          expect.objectContaining({
+            name: 'BandAid',
+            quantity: '18',
+            purchaseDate: '11/30/2022'
+          })
+        ]);
+      });
+    });
+  });
+});
 // });
--- controllers/__tests__/foodTest.js
+++ controllers/__tests__/foodTest.js
@@ -1,89 +1,88 @@
-const foodController = require("../food");
-const mongoose = require("mongoose");
+const foodController = require('../food');
+const mongoose = require('mongoose');
+const mockingoose = require('mockingoose');
 
 let req, res, send;
 
 beforeEach(() => {
-    send = jest.fn();
-    req = {};
-    res =  {
-        status: jest.fn(() => ({send})),
-        json: jest.fn(),
-    };
+  send = jest.fn();
+  req = {};
+  res = {
+    status: jest.fn(() => ({ send })),
+    json: jest.fn()
+  };
 });
 
-describe("getAllFood()", () => {
-    describe("when there is no user", () => {
-        beforeEach(()  => (req.user = undefined));
-        
-        it("responds with 401", () => {
-            foodController.index(req, res);
+describe('getAllFood()', () => {
+  describe('when there is no user', () => {
+    beforeEach(() => (req.user = undefined));
 
-            expect(res.status).toHaveBeenCalledWith(401);
-        });
+    it('responds with 401', () => {
+      foodController.index(req, res);
 
-        it("responds with 'Not Authenticated'",() => {
-            foodController.index(req,res);
-
-            expect(send).toHaveBeenCalledWith("Not Authenticated");
-        });
+      expect(res.status).toHaveBeenCalledWith(401);
     });
 
-    describe("when there is a user present",() => {
+    it("responds with 'Not Authenticated'", () => {
+      foodController.index(req, res);
+
+      expect(send).toHaveBeenCalledWith('Not Authenticated');
+    });
+  });
+
+  describe('when there is a user present', () => {
+    beforeEach(() => {
+      req.user = {
+        identifer: 'testUser'
+      };
+    });
+
+    describe('when food array is empty', () => {
+      beforeEach(() => {
+        data = [];
+      });
+
+      it('responds with 200', () => {
+        foodController.getAllFood(req, res);
+
+        expect(res.status).toHaveBeenCalledWith([]);
+      });
+      describe('when food items exists', () => {
         beforeEach(() => {
-            req.user = {
-                identifer: "testUser",
-            };
+          data = [
+            {
+              foodName: 'pasta',
+              brandName: 'Great Value',
+              quanitity: '10',
+              purchaseDate: '12/25/2000',
+              expirationDate: '12/25/2010',
+              orderNextByDate: '12/25/2009',
+              description: 'It is pasta'
+            }
+          ];
         });
 
-        describe("when food array is empty", () => {
-            beforeEach(() => {
-                data = [];
-            });
-
-            it("responds with 200", () =>{
-                foodController.getAllFood(req,res);
-
-                expect(res.status).toHaveBeenCalledWith([]);
-            });
-            describe("when food items exists", () => {
-                beforeEach(() => {
-                    data = [
-                        {
-                            foodName: "pasta",
-                            brandName: "Great Value",
-                            quanitity: "10",
-                            purchaseDate: "12/25/2000",
-                            expirationDate: "12/25/2010",
-                            orderNextByDate: "12/25/2009",
-                            description: "It is pasta",
-
-                        }
-                        
-                    ];
-                });
-
-                it("responds with 200", () => {
-                    foodController.getAllFood(req,res);
-                    expect(res.status).toHaveBeenCalledWith(200);
-                });
-
-                it("responds with food items", () => {
-                    foodController.getAllFood(req,res);
-
-                    expect(send).toHaveBeenCalledWith([
-                        {
-                            foodName: "pasta",
-                            brandName: "Great Value",
-                            quanitity: "10",
-                            purchaseDate: "12/25/2000",
-                            expirationDate: "12/25/2010",
-                            orderNextByDate: "12/25/2009",
-                            description: "It is pasta",
-                        }
-                    ]);
-                });
-            });
+        it('responds with 200', () => {
+          foodController.getAllFood(req, res);
+          expect(res.status).toHaveBeenCalledWith(200);
         });
+
+        it('responds with food items', () => {
+          foodController.getAllFood(req, res);
+
+          expect(send).toHaveBeenCalledWith([
+            {
+              foodName: 'pasta',
+              brandName: 'Great Value',
+              quanitity: '10',
+              purchaseDate: '12/25/2000',
+              expirationDate: '12/25/2010',
+              orderNextByDate: '12/25/2009',
+              description: 'It is pasta'
+            }
+          ]);
+        });
+      });
     });
+  });
 });
