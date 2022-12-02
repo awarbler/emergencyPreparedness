dabd237602e4ac979e7ee421f9cb454a6ae40d12
Weston Dransfield <wdrans@gmail.com>
2022-12-01 20:40:22 -0700

    Add mockingoose and async code control in one broken test

--- controllers/__tests__/firstAidTest.js
+++ controllers/__tests__/firstAidTest.js
@@ -1,88 +1,101 @@
 const firstAidController = require("../firstAid");
 const mongoose = require("mongoose");
+const mockingoose = require("mockingoose");
+const FirstAid = require("../../models/firstAid");
 
 let req, res, send;
 
 beforeEach(() => {
-    send = jest.fn();
+  send = jest.fn();
 
-    req = {};
-    res = {
-        status: jest.fn(() => ({ send })),
-        json: jest.fn(),
-    };
+  req = {};
+  res = {
+    status: jest.fn(() => ({ send })),
+    json: jest.fn(),
+  };
 });
 
 describe("getAllFirstAidItems()", () => {
-    describe("when there is no user present", () => {
-        beforeEach(() => (req.user = undefined));
+  describe("when there is no user present", () => {
+    beforeEach(() => (req.user = undefined));
 
-        it("responds with 401",  () => {
-            firstAidController.getAllFirstAidItems(req, res);
-    
-            expect(res.status).toHaveBeenCalledWith(401);
-        });
-    
-        it("responds with 'Not Authenticated'", () => {
-            firstAidController.getAllFirstAidItems(req, res);
-    
-            expect(send).toHaveBeenCalledWith("Not Authenticated");
-        });
+    it("responds with 401", () => {
+      firstAidController.getAllFirstAidItems(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
     });
-    describe("when there is a user present", () => {
+
+    it("responds with 'Not Authenticated'", () => {
+      firstAidController.getAllFirstAidItems(req, res);
+
+      expect(send).toHaveBeenCalledWith("Not Authenticated");
+    });
+  });
+  describe("when there is a user present", () => {
+    beforeEach(() => {
+      req.user = {
+        identifier: "testUser",
+      };
+    });
+
+    describe("when firstAid array is empty", () => {
+      beforeEach(() => {
+        data = [];
+      });
+
+      it("responds with 200", () => {
+        firstAidController.getAllFirstAidItems(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+      });
+
+      it("responds with an empty array", () => {
+        firstAidController.getAllFirstAidItems(req, res);
+
+        expect(send).toHaveBeenCalledWith([]);
+      });
+
+      describe("when firstAid items exist", () => {
         beforeEach(() => {
-            req.user = {
-                identifier: "testUser",
-            };
-        });
-    
-        describe("when firstAid array is empty", () => {
-            beforeEach(() => {
-                data = [];
-            });
-    
-            it("responds with 200", () => {
-                firstAidController.getAllFirstAidItems(req, res);
-    
-                expect(res.status).toHaveBeenCalledWith(200);
-            });
-    
-            it("responds with an empty array", () => {
-                firstAidController.getAllFirstAidItems(req, res);
-    
-                expect(send).toHaveBeenCalledWith([]);
-            });
-    
-            describe("when firstAid items exist", () => {
-                beforeEach(() => {
-                    data = [
-                        {
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }
-                    ];
-                });
-    
-                it("responds with 200", () => {
-                    firstAidController.getAllFirstAidItems(req, res);
-    
-                    expect(res.status).toHaveBeenCalledWith(200);
-                });
-    
-                it("responds with firstAid items", () => {
-                    firstAidController.getAllFirstAidItems(req, res);
-    
-                    expect(send).toHaveBeenCalledWith([
-                        {
-                            name: "BandAid",
-                            quantity: "18",
-                            purchaseDate: "11/30/2022",
-                        }
-                    ]);
-                });
-            });
-        });
-    });
-});
+          data = [
+            {
+              name: "BandAid",
+              quantity: "18",
+              purchaseDate: "11/30/2022",
+            },
+          ];
 
+          mockingoose(FirstAid).toReturn(
+            [
+              {
+                name: "BandAid",
+                quantity: "18",
+                purchaseDate: "11/30/2022",
+              },
+            ],
+            "find"
+          );
+        });
+
+        it("responds with 200", () => {
+          firstAidController.getAllFirstAidItems(req, res);
+
+          expect(res.status).toHaveBeenCalledWith(200);
+        });
+
+        it("responds with firstAid items", async () => {
+          await firstAidController.getAllFirstAidItems(req, res);
+
+          console.log("2. Doing the assertion with 'expect'");
+          expect(send).toHaveBeenCalledWith([
+            expect.objectContaining({
+              name: "BandAid",
+              quantity: "18",
+              purchaseDate: "11/30/2022",
+            }),
+          ]);
+        });
+      });
+    });
+  });
+});
--- controllers/firstAid.js
+++ controllers/firstAid.js
@@ -1,4 +1,4 @@
-const FirstAid = require('../models/firstAid');
+const FirstAid = require("../models/firstAid");
 
 // const getFavFirstAidStorage = async (req, res) => {
 //   try {
@@ -12,19 +12,22 @@ const FirstAid = require('../models/firstAid');
 //   }
 // };
 
-const getAllFirstAidItems = async (req, res) => {
+const getAllFirstAidItems = (req, res) => {
   try {
     if (!req.user) {
       return res.status(401).send("Not Authenticated");
     }
 
-    FirstAid.find({})
+    return FirstAid.find({})
       .then((data) => {
+        console.log("1. Calling 'send'");
         res.status(200).send(data);
       })
       .catch((err) => {
         res.status(500).send({
-          message: err.message || 'Some error occurred while retrieving first-aid items.'
+          message:
+            err.message ||
+            "Some error occurred while retrieving first-aid items.",
         });
       });
   } catch (err) {
@@ -33,120 +36,132 @@ const getAllFirstAidItems = async (req, res) => {
 };
 
 const getFirstAidItemByName = async (req, res) => {
-    try {
-      if (!req.user) {
-        return res.status(401).send("Not Authenticated");
-      }
-
-      const name = req.params.name;
-      
-      if (!name) {
-        res.status(400).send('Must include name.')
-      }
-      
-      FirstAid.find({ name : name })
-        .then((data) => {
-          res.status(200).send(data[0]);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            message: err.message || 'Some error occurred while retrieving first-aid item.'
-          });
-        });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  try {
+    if (!req.user) {
+      return res.status(401).send("Not Authenticated");
     }
-  };
 
+    const name = req.params.name;
 
-  const createNewFirstAidItem = async (req, res) => {
-    try {
-      if (!req.user) {
-        return res.status(401).send("Not Authenticated");
-      }
-
-      if (!req.body.name || !req.body.quantity || !req.body.purchaseDate) {
-        res.status(400).send({ message: 'Input can not be empty!' });
-        return;
-      }
-
-      const firstAid = new FirstAid(req.body);
-      firstAid
-        .save()
-        .then((data) => {
-          console.log(data);
-          res.status(201).send(data);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            message: err.message || 'Some error occurred while creating the first-aid item.'
-          });
-        });
-    } catch (err) {
-      res.status(500).json(err);
+    if (!name) {
+      res.status(400).send("Must include name.");
     }
-  };
-  
-  const updateFirstAidItem = async (req, res) => {
-    try {
-      if (!req.user) {
-        return res.status(401).send("Not Authenticated");
-      }
-      const name = req.params.name;
+
+    FirstAid.find({ name: name })
+      .then((data) => {
+        res.status(200).send(data[0]);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message ||
+            "Some error occurred while retrieving first-aid item.",
+        });
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const createNewFirstAidItem = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).send("Not Authenticated");
+    }
+
+    if (!req.body.name || !req.body.quantity || !req.body.purchaseDate) {
+      res.status(400).send({ message: "Input can not be empty!" });
+      return;
+    }
+
+    const firstAid = new FirstAid(req.body);
+    firstAid
+      .save()
+      .then((data) => {
+        console.log(data);
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message ||
+            "Some error occurred while creating the first-aid item.",
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updateFirstAidItem = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).send("Not Authenticated");
+    }
+    const name = req.params.name;
     //   if (!name) {
     //     res.status(400).send({ message: 'Invalid name Supplied' });
     //     return;
     //   }
 
     if (!req.body.name || !req.body.quantity || !req.body.purchaseDate) {
-        res.status(400).send({ message: 'Input can not be empty!' });
-        return;
-      }
-
-  
-      FirstAid.findOne({ name: name }, function (err, firstAid) {
-        firstAid.name = req.body.name;
-        firstAid.quantity = req.body.quantity;
-        firstAid.purchaseDate = req.body.purchaseDate;
-        firstAid.save(function (err) {
-          if (err) {
-            res.status(500).json(err || 'Some error occurred while updating the first-aid item.');
-          } else {
-            res.status(204).send();
-          }
-        });
-      });
-    } catch (err) {
-      res.status(500).json(err);
+      res.status(400).send({ message: "Input can not be empty!" });
+      return;
     }
-    
-  };
-  
-  
-  const deleteFirstAidItem = async (req, res) => {
-    try {
-      if (!req.user) {
-        return res.status(401).send("Not Authenticated");
-      }
-      const name = req.params.name;
-  
-      if (!name) {
-        res.status(400).send({ message: 'Name Invalid' });
-        return;
-      }
-  
-      FirstAid.deleteOne({ name: name }, (err, result) =>  {
+
+    FirstAid.findOne({ name: name }, function (err, firstAid) {
+      firstAid.name = req.body.name;
+      firstAid.quantity = req.body.quantity;
+      firstAid.purchaseDate = req.body.purchaseDate;
+      firstAid.save(function (err) {
         if (err) {
-          res.status(500).json(err || 'Some error occurred while deleting the first-aid item.');
+          res
+            .status(500)
+            .json(
+              err || "Some error occurred while updating the first-aid item."
+            );
         } else {
-          res.status(200).send(result);
+          res.status(204).send();
         }
       });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-    } catch (err) {
-      res.status(500).json(err);
+const deleteFirstAidItem = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).send("Not Authenticated");
     }
-  };
-  
+    const name = req.params.name;
 
-module.exports = { getAllFirstAidItems, getFirstAidItemByName, createNewFirstAidItem, updateFirstAidItem, deleteFirstAidItem };
+    if (!name) {
+      res.status(400).send({ message: "Name Invalid" });
+      return;
+    }
+
+    FirstAid.deleteOne({ name: name }, (err, result) => {
+      if (err) {
+        res
+          .status(500)
+          .json(
+            err || "Some error occurred while deleting the first-aid item."
+          );
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = {
+  getAllFirstAidItems,
+  getFirstAidItemByName,
+  createNewFirstAidItem,
+  updateFirstAidItem,
+  deleteFirstAidItem,
+};
