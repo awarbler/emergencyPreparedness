e2a11bbf13fce9be730474ef6004f050dd95f29a
[awarbler] <[awoodfor@byui.edu]>
2022-12-01 22:29:17 -0600

    edited and added testing

--- controllers/__tests__/hygieneTest.js
+++ controllers/__tests__/hygieneTest.js
@@ -1,48 +1,87 @@
-const hygieneController = require("../hygiene.js");
-const mongoose = require("mongoose");
+const hygieneController = require('../firstAid');
+const mongoose = require('mongoose');
 
 let req, res, send;
 
 beforeEach(() => {
-    send = jest.fn();
+  send = jest.fn();
 
-    req = {};
-    res ={
-        status: jest.fn(() => ({ send })),
-        json: jest.fn()
-    };
+  req = {};
+  res = {
+    status: jest.fn(() => ({ send })),
+    json: jest.fn()
+  };
 });
-describe ("getAllHygienes()",() => {
-    describe (" a list of all hygiene assigned to a user",() => {
-        beforeEach(() => (req.user = underfined));
 
-        it ("responds with 401", () => {
-            hygiene.index(req, res);
+describe('getAllHygienes()', () => {
+  describe('when there is no user present', () => {
+    beforeEach(() => (req.user = undefined));
 
-            expect(res.status).toHaveBeenCalledWith(401);
-        });
-        it("responds with 'Not Authenticated", () => {
-            hygieneController.index(req, res);
-            expect(send).toHaveBeenCalledWith("Not Authenticated");
-        });
+    it('responds with 401', () => {
+      hygieneController.getAllHygienes(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
     });
-    describe ("when there is a user present", () => {
+
+    it("responds with 'Not Authenticated'", () => {
+      hygieneController.getAllHygienes(req, res);
+
+      expect(send).toHaveBeenCalledWith('Not Authenticated');
+    });
+  });
+  describe('when there is a user present', () => {
+    beforeEach(() => {
+      req.user = {
+        identifier: 'testUser'
+      };
+    });
+
+    describe('when firstAid array is empty', () => {
+      beforeEach(() => {
+        data = [];
+      });
+
+      it('responds with 200', () => {
+        hygieneController.getAllHygienes(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+      });
+
+      it('responds with an empty array', () => {
+        hygieneController.getAllHygienes(req, res);
+
+        expect(send).toHaveBeenCalledWith([]);
+      });
+
+      describe('when firstAid items exist', () => {
         beforeEach(() => {
-            req.user = {
-                identifier: "testUser",
-            };
+          data = [
+            {
+              hygieneName: 'Dial Soap',
+              hygineQuantity: '18',
+              hygienPurchaseDate: '11/30/2022'
+            }
+          ];
         });
-        // describe("when the user has no hygiene items", () => {
-        //     beforeEach(() => (
-        //         req.user = underfined));
 
-        //     it("responds with 401", () => {
-        //         hygieneC
-        //     })
+        it('responds with 200', () => {
+          hygieneController.getAllHygienes(req, res);
 
-        // })
+          expect(res.status).toHaveBeenCalledWith(200);
+        });
 
-        
+        it('responds with firstAid items', () => {
+          hygieneController.getAllHygienes(req, res);
+
+          expect(send).toHaveBeenCalledWith([
+            {
+              hygieneName: 'Dial Soap',
+              hygineQuantity: '18',
+              hygienPurchaseDate: '11/30/2022'
+            }
+          ]);
+        });
+      });
     });
-
+  });
 });
