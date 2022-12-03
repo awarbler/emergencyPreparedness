aa81ecf228adee9391c31ff003864e519d89f295
ohsaraho <osg21001@byui.edu>
2022-12-03 13:15:52 -0700

    firstAid test routes completed

--- routes/__tests__/firstAid.test.js
+++ routes/__tests__/firstAid.test.js
@@ -1,12 +1,37 @@
-const { route } = require("../firstAid");
 const firstAidRouter = require("../firstAid");
 const routesFor = require("./routesFor");
 
+const routes = routesFor(firstAidRouter);
+
 describe("routes", () => {
-    it("testing routes with console.log", () => {
-        // console.log(firstAidRouter);
-        // console.log(firstAidRouter.stack[1].route);
-        // routesFor(firstAidRouter);
-        console.log(routesFor(firstAidRouter));
-    })
+    // it("testing routes with console.log", () => {
+    //     console.log(firstAidRouter);
+    //     console.log(firstAidRouter.stack[1].route);
+    //     routesFor(firstAidRouter);
+    //     console.log(routesFor(firstAidRouter));
+    // })
+    describe("/", () => {
+        it("supports http GET request", () => {
+            expect(routes["/"]).toContain("get");
+        });
+    
+        it("supports http POST request", () => {
+            expect(routes["/"]).toContain("post");
+        });
+    });
+    
+    describe("/:name", () => {
+        it("supports http GET request", () => {
+            expect(routes["/:name"]).toContain("get");
+        });
+    
+        it("supports http PUT request", () => {
+            expect(routes["/:name"]).toContain("put");
+        });
+
+        it("supports http DELETE request", () => {
+            expect(routes["/:name"]).toContain("delete");
+        });
+    });
+    
 })
