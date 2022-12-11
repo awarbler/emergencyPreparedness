ec991d9872e4e597c8e1c4ea1b6e0040bfacac02
[awarbler] <[awoodfor@byui.edu]>
2022-12-10 20:59:32 -0600

    fixing routes on hygiene

--- routes/hygiene.js
+++ routes/hygiene.js
@@ -1,14 +1,6 @@
 const express = require("express");
-const { check } = require("express-validator");
 const router = express.Router();
 const mongoose = require("mongoose");
-// const { body, param } = require('express-validator');
-// const {
-//     createValidator,
-//     showValidator,
-//     updateValidator,
-//     destroyValidator
-// } = require('../validators/hygieneValidator.js');
 const hygieneController = require("../controllers/hygiene");
 const loadUser = require("../middleware/loadUser");
 
@@ -17,24 +9,8 @@ router.use([loadUser]);
 router.get("/", hygieneController.getAllHygienes);
 router.get("/:name", hygieneController.getHygieneByName);
 router.get("/:id", hygieneController.getHygieneById);
-router.post(
-  "/", // validate that the name is not empty
-  [
-    check("name").not().isEmpty(),
-    check("quanitiy").not().isEmpty(),
-    check("purchaseDate").not().isEmpty()
-  ],
-  hygieneController.createNewHygiene
-);
-router.put(
-  "/:name",
-  [
-    check("name").not().isEmpty(),
-    check("quanitiy").not().isEmpty(),
-    check("purchaseDate").not().isEmpty()
-  ],
-  hygieneController.updateHygiene
-);
+router.post("/", hygieneController.createNewHygiene);
+router.put("/:name", hygieneController.updateHygiene);
 router.delete("/:name", hygieneController.deleteHygiene);
 
 module.exports = router;
