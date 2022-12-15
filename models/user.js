8548f181116ec96e70de09d33d070ef180f8c764
Anita Woodford <55596748+awarbler@users.noreply.github.com>
2022-12-14 23:07:33 -0600

    clean up comments from code

--- models/user.js
+++ models/user.js
@@ -1,17 +1,8 @@
 const { Schema, model } = require("mongoose");
-// const HygieneSchema = require("./hygiene").Schema;
-// const firstAidSchema = require("./firstAid ");
-// const foodSchema = require(".")
+
 
 const userSchema = new Schema({
-  //   identifier: { type: String, unique: true },
-  //   userName: { type: String },
-  //   email: { type: String, unique: true },
-  //   password: { type: String },
-  //   givenName: { type: String },
-  //   familyName: { type: String },
-  //   locale: { type: String },
-  //   picture: { type: String },
+  
   identifier: { type: String, unique: true },
   email: { type: String, unique: true },
   givenName: { type: String },
